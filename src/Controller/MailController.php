bd1c175d64a2f19cdceaf10d73a57bd3c464e0d9
[Thomas <sonnleitner.thomas@gmx.at>
2023-04-25 15:13:55 +0200

    mailerSecondtry

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -11,12 +11,12 @@ use Symfony\Component\Mime\Email;
 class MailController extends AbstractController
 {
     #[Route('/mail', name: 'app_mail')]
-    public function sendEmail(MailerInterface $mailer): Response
+    public function sendEmail(MailerInterface $mailer, $maili): Response
     {
         $email = (new Email())
             ->from('sontomson1@gmail.com')
-            ->to('sonnleitner.thomas@gmx.at') // $this->getUser()->getEMail()
-            // ->cc('sonnleitner.thomas@gmx.at')
+            ->to($maili) // $this->getUser()->getEMail()
+            ->cc('sonnleitner.thomas@gmx.at')
             //->bcc('bcc@example.com')
             //->replyTo('fabien@example.com')
             //->priority(Email::PRIORITY_HIGH)
@@ -25,6 +25,7 @@ class MailController extends AbstractController
             ->text('Your order from OnlyFans!')
             ->html('<p>Thanks for your order. Your products will be shipped in the next few days!</p>');
 
+        $maili->getUser()->getEMail();
         $mailer->send($email);
         return new Response('Order successfully submitted. You will shortly receive a confirmation email.');
         // ...
